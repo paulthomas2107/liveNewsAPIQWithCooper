5189c76d78493a7559ea3e58590efc8eba460cf5
Paul Thomas <paulthomas2107@gmail.com>
2022-11-20 10:33:12 +0000

    More....

--- server.ts
+++ server.ts
@@ -2,6 +2,14 @@ import express from 'express';
 import { createServer } from 'http';
 import { PubSub } from 'graphql-subscriptions';
 import gql from 'graphql-tag';
+import { WebSocketServer } from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { ApolloServer } from '@apollo/server';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { expressMiddleware } from '@apollo/server/express4';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 (async function () {
   // Server
@@ -34,29 +42,67 @@ import gql from 'graphql-tag';
   }
 
   const resolvers = {
-    Query: {
-      placeholder: () => {
+    Query: {    
+      placeHolder: () => {
         return true;
       },
     },
     Mutation: {
       createNewsEvent: (_parent: any, args: createNewsEventInput) => {
-        console.log(args)
-        pubSub.publish('EVENT_CREATED', {newsFeed: args})
+        console.log(args);
+        pubSub.publish('EVENT_CREATED', { newsFeed: args });
         // Persist to DB can go here...
-        return args
+        return args;
       },
     },
     Subscription: {
-        newsFeed: {
-            subscribe: () => pubSub.asyncIterator(['EVENT_CREATED'])
-        }
-    }
+      newsFeed: {
+        subscribe: () => pubSub.asyncIterator(['EVENT_CREATED']),
+      },
+    },
   };
+
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+
   // Start websocket server
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: '/graphql', // localhost:3000/graphql
+  });
+
+  const serverCleanup = useServer({ schema }, wsServer); // dispose / teardown server
+
   // Create Apollo Server
+  const server = new ApolloServer({
+    schema,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
+    ],
+  });
+
   // Start Server
+  await server.start();
+
   // Apply middlewares (cors, express)
+  app.use(
+    '/graphql',
+    cors<cors.CorsRequest>(),
+    bodyParser.json(),
+    expressMiddleware(server)
+  );
+
   // HttpServer start
+  httpServer.listen(4000, () => {
+    console.log("Server running on http://localhost:4000/graphql")
+  });
 
 })();
